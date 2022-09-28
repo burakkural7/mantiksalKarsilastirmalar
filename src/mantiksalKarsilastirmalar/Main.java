39000228ca6e9cbb91bd2a9e373f6a3c0386f221
burakkural7 <113901994+burakkural7@users.noreply.github.com>
2022-09-28 12:03:42 +0300

    Initial commit

---
+++ src/mantiksalKarsilastirmalar/Main.java
@@ -0,0 +1,34 @@
+package mantiksalKarsilastirmalar;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double product1 = 14.99;
+		double product2 = 12.49;
+		String mutluYuz = ":)";
+		String üzgünYuz = ":)";
+		if (product1 == product2) {
+			System.out.println("eşit");
+		}
+		if (product1 < product2) {
+			System.out.println("1. ürün daha ucuz");
+		}
+		if (product2 < product1) {
+			
+		}
+		
+		
+		// yüz ifadeleri başlangıç
+					String yuzIfadesi = "mutlu";
+					if (yuzIfadesi == "mutlu" || yuzIfadesi == "MUTLU" || yuzIfadesi == "Mutlu")
+						;
+					System.out.println("(:");
+					{
+					}
+					if (yuzIfadesi == "üzgün" || yuzIfadesi == "ÜZGÜN" || yuzIfadesi == "Üzgün")
+						;
+					System.out.println("):");
+					// yüz ifadeleri bitiş
+
+	}
+}
